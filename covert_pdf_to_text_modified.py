3b7694e1ea4586b1f3bcd1901597984002873bd3
loki3789 <40861505+loki3789@users.noreply.github.com>
2018-07-26 15:12:52 +0530

    Create covert_pdf_to_text_modified.py

---
+++ covert_pdf_to_text_modified.py
@@ -0,0 +1,25 @@
+# importing required modules
+import PyPDF2
+
+# creating a pdf file object
+pdfFileObj = open('3.pdf', 'rb')
+
+# creating a pdf reader object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+# printing number of pages in pdf file
+number_of_pages=pdfReader.numPages
+index_of_page=0
+fhand=open("data.txt","w",encoding='utf8')
+# creating a page object
+for page in range(0,number_of_pages):
+
+  pageObj = pdfReader.getPage(index_of_page)
+
+# extracting text from page
+  fhand.write(pageObj.extractText())
+  index_of_page=index_of_page+1
+
+
+# closing the pdf file object
+pdfFileObj.close()
